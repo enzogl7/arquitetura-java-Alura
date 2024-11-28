56cad546002a7c0a43375b8bea0e9d81160f4e72
Enzo Lima <enzolima527@gmail.com>
2024-11-28 15:33:12 -0300

    .

--- 3698-java-clean-architecture-main/src/main/java/br/com/alura/codechella/infra/persistence/UsuarioEntity.java
+++ 3698-java-clean-architecture-main/src/main/java/br/com/alura/codechella/infra/persistence/UsuarioEntity.java
@@ -1,5 +1,6 @@
-package br.com.alura.codechella.naousar.model;
+package br.com.alura.codechella.infra.persistence;
 
+import br.com.alura.codechella.domain.entities.usuario.Usuario;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,7 +11,7 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "usuarios")
-public class Usuario {
+public class UsuarioEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
@@ -58,4 +59,16 @@ public class Usuario {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public UsuarioEntity() {
+
+    }
+
+    public UsuarioEntity(String cpf, String nome, LocalDate nascimento, String email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.email = email;
+    }
 }
+
--- 3698-java-clean-architecture-main/src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
+++ 3698-java-clean-architecture-main/src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -1,5 +1,6 @@
 package br.com.alura.codechella.domain.entities.usuario;
 
+import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +14,20 @@ public class UsuarioTest {
                         "Enzo",
                         LocalDate.parse("1990-12-12"),
                         "email@email.com"));
+    }
+
+    @Test
+    public void deveCriarUsuarioComAFabrica() {
+        FabricaDeUsuario fabrica = new FabricaDeUsuario();
+        Usuario usuario = fabrica.comNomeCpfNascimento
+                ("Enzo",
+                "123.456.789-22",
+                LocalDate.parse("1993-10-10"));
+
+        Assertions.assertEquals("Enzo", usuario.getNome());
+
+        usuario = fabrica.incluiEndereco("12345-999", 40, "apto 201");
+
+        Assertions.assertEquals("apto 201", usuario.getEndereco().getComplemento());
     }
 }
